46f8619fb0e9dc71bbaafae09ea5aa96c3c80a95
wonsu0513 <wonsu0513@gmail.com>
2021-08-31 19:52:56 -0400

    updated files

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
+from glob import glob
 from setuptools import setup
 
-package_name = 'ros2-foxy-wearable-biosensors'
+package_name = 'ros-foxy-biosensors'
 
 setup(
     name=package_name,
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'Emotiv_Insight_Node = ros-foxy-biosensors.emotiv_insight_node:main',
+            'Empatica_E4_Node = ros-foxy-biosensors.empatica_e4_node:main',
         ],
     },
 )
